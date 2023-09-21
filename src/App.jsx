c2aa14df94875f097c3cb8b1be31a2d58492adef
vapppu <veera.hiltunen@gmail.com>
2023-09-21 11:51:57 +0300

    Return 'Hello world'

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div>Hello world</div>
+  )
+}
+
+export default App
