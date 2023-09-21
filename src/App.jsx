bb22306940eb3671aaff815a787ae2a756d9bb1f
vapppu <veera.hiltunen@gmail.com>
2023-09-21 14:40:19 +0300

    Add bornYear function to Hello component to calculate the person's birth year

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
 }
