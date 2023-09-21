80c3d6ef50e3022c64466e128a41946328e933dd
vapppu <veera.hiltunen@gmail.com>
2023-09-21 14:38:26 +0300

    Render two Hello components that greet the person and display their age

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
-  const friends = [
-    { name: 'Leevi', age: 4 },
-    { name: 'Venla', age: 10 },
-  ]
+  const nimi = 'Pekka'
+  const ika = 10
 
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
     </div>
   )
 }
